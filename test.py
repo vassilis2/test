2e2484dfc931f342b9d11fdf6eae5eaa432bb823
vassilis2 <kollatos@pakia.io>
2020-01-21 16:49:51 +0200

    PUT problem

--- test.py
+++ test.py
@@ -17,14 +17,21 @@ def listing():
 
 @app.route('/books', methods=['POST'])
 def entering():
-    book = {
+  book = {
     'title': request.json['title'],
     'author': request.json['author'],
     'ISBN' : request.json['ISBN'],
     'description' : request.json['description']
     } 
-    books.append(book)
-    return jsonify({'books' : books}), 201
+  books.append(book)
+  return jsonify({'books' : books}), 201
+
+@app.route('/books/<title>', methods=['PUT'])
+def updating(title):
+  for kati in books:
+    req = request.get_json()
+    books[-1].update(req)
+    return "The book {} updated".format(title), 201
 
 @app.route('/books/<title>', methods=['DELETE'])
 def deleting(title):
