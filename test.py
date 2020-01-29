d34c77098f4aff654f2ae15d89a9286d97321597
vassilis2 <kollatos@pakia.io>
2020-01-29 15:39:10 +0200

    all CRUD final

--- test.py
+++ test.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, json
 
 app = Flask(__name__)
 app.config['JSON_SORT_KEYS'] = False
@@ -10,8 +10,11 @@ def listing():
 
 @app.route('/books/<title>', methods=['GET'])
 def get_one(title):
-  book = [book for book in books if book['title'] == title]
-  return jsonify(book), 200
+  if not filter(lambda book: book['title'] == title, books):
+    return "There is no {} book in the list".format(title), 404
+  else:  
+    book = [book for book in books if book['title'] == title]
+    return jsonify(book), 200
 
 @app.route('/books', methods=['POST'])
 def adding():
@@ -21,20 +24,24 @@ def adding():
 
 @app.route('/books/<title>', methods=['PUT'])
 def updating(title):
-
-  book_to_update = jsonify([book for book in books if book['title'] == title])
-  
-  incoming_json_data = request.get_json()
-  
-  for key in incoming_json_data:
-    book[key] = incoming_json_data[key]
-  return "The book {} updated".format(title), 200
+  if filter(lambda book: book['title'] == title, books):
+    incoming_json_data = request.get_json()
+    for key, value in tuple(incoming_json_data.items()):
+      for book in books:
+        if book['title'] == title:
+          book[str(key)] = incoming_json_data.get(key, None)
+    return "The book {} updated".format(title), 200
+  else:
+    return "The book {} isn't in the list".format(title), 404
 
 @app.route('/books/<title>', methods=['DELETE'])
 def deleting(title):
-  book = [book for book in books if book['title'] == title]
-  books.remove(book[0])
-  return "The book {} deleted".format(title), 200
+  if not filter(lambda book: book['title'] == title, books):
+    return "The book {} isn't in the list".format(title), 404
+  else:
+    book = [book for book in books if book['title'] == title]
+    books.remove(book[0])
+    return "The book {} deleted".format(title), 200
 
 if __name__ == '__main__':
   app.run(debug=True)
